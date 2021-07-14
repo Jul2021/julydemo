9cb60c867f2157a72a9706a417c8a15ca12b3d12
KK <kiranops@yahoo.com>
2021-07-14 07:34:52 +0530

    Added descriptive comments

--- readvars.sh
+++ readvars.sh
@@ -2,7 +2,12 @@
 
 source ./vars
 
+
+echo "All these files are read from vars file."
+echo "If you don't have one create vars file and update the variables"
 echo $resourceGroup
 echo $vnetName
 echo $vmname
 echo $webNsg
+
+
